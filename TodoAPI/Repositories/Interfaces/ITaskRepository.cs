fe0ef58c3b947589dd6fea6dac574c66aac71eb6
J_Nam <NamltGCD18728@fpt.edu.vn>
2021-12-18 15:20:59 +0700

    Implement task repository

---
+++ TodoAPI/Repositories/Interfaces/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoAPI.Models;
+
+namespace TodoAPI.Repositories.Interfaces
+{
+    interface ITaskRepository
+    {
+        ICollection<Task> GetTasks();
+        Task GetTask(int id);
+        bool CreateTask(Task task);
+        bool UpdateTask(Task task);
+        bool DelteTask(Task task);
+        bool TaskExist(string name);
+        bool Save();
+    }
+}
---
+++ TodoAPI/Repositories/TaskRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoAPI.Data;
+using TodoAPI.Repositories.Interfaces;
+
+namespace TodoAPI.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool CreateTask(Models.Task task)
+        {
+            _context.Tasks.Add(task);
+            return Save();
+        }
+
+        public bool DelteTask(Models.Task task)
+        {
+            _context.Tasks.Remove(task);
+            return Save();
+        }
+
+        public Models.Task GetTask(int id)
+        {
+            var taskInDb = _context.Tasks.SingleOrDefault(t => t.TaskId == id);
+            return taskInDb;
+        }
+
+        public ICollection<Models.Task> GetTasks()
+        {
+            return _context.Tasks.OrderBy(o => o.TaskName).ToList();
+        }
+
+        public bool Save()
+        {
+            return _context.SaveChanges() >= 0 ? true: false;
+        }
+
+        public bool TaskExist(string name)
+        {
+            var taskExist = _context.Tasks.Any(n => n.TaskName.ToLower() == name.ToLower().Trim());
+            return taskExist;
+        }
+
+        public bool UpdateTask(Models.Task task)
+        {
+            _context.Tasks.Update(task);
+            return Save();
+        }
+    }
+}
