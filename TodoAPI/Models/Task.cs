82c04d92b7f66babda712ed1758ae7a5eb0fbeb6
J_Nam <NamltGCD18728@fpt.edu.vn>
2021-12-18 14:38:04 +0700

    Create Task model

---
+++ TodoAPI/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        [Required]
+        public DateTime CreateAt { get; set; }
+        public bool Status { get; set; }
+        public string Description { get; set; }
+    }
+}
